52f60a12bf727af526cb9c8bde9890d2bf84e580
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2025-05-14 18:01:29 +0200

    tunnel: docs: Document NetBuffer

--- api/tunnel.go
+++ api/tunnel.go
@@ -14,15 +14,23 @@ import (
 	"golang.zx2c4.com/wireguard/tun"
 )
 
+// NetBuffer is a pool of byte slices with a fixed capacity.
+// Helps to reduce memory allocations and improve performance.
+// It uses a sync.Pool to manage the byte slices.
+// The capacity of the byte slices is set when the pool is created.
 type NetBuffer struct {
 	capacity int
 	buf      sync.Pool
 }
 
+// Get returns a byte slice from the pool.
 func (n *NetBuffer) Get() []byte {
 	return n.buf.Get().([]byte)
 }
 
+// Put places a byte slice back into the pool.
+// It checks if the capacity of the byte slice matches the pool's capacity.
+// If it doesn't match, the byte slice is not returned to the pool.
 func (n *NetBuffer) Put(buf []byte) {
 	if cap(buf) != n.capacity {
 		return
@@ -30,6 +38,8 @@ func (n *NetBuffer) Put(buf []byte) {
 	n.buf.Put(buf)
 }
 
+// New creates a new NetBuffer with the specified capacity.
+// The capacity must be greater than 0.
 func (n *NetBuffer) New(capacity int) {
 	if capacity <= 0 {
 		panic("capacity must be greater than 0")
